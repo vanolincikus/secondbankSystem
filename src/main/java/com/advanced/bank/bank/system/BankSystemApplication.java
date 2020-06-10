6a45867e2ad72876930e9bb1f63b19c7dd9d6a13
Paskal <paskal.novakov@gmail.com>
2020-06-10 19:02:15 +0300

    initial commit

---
+++ src/main/java/com/advanced/bank/bank/system/BankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.advanced.bank.bank.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advanced/bank/bank/system/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced.bank.bank.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
