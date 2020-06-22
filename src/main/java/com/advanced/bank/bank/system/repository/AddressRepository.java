82d5e233a4fa1c1a87f73a5cf42c6c0635baf4fe
5yapetkova <ppetkova25@gmail.com>
2020-06-22 20:09:14 +0300

    last update

--- src/main/java/com/advanced/bank/bank/system/repository/AddressRepository.java
+++ src/main/java/com/advanced/bank/bank/system/repository/AddressRepository.java
@@ -3,7 +3,9 @@ package com.advanced.bank.bank.system.repository;
 import com.advanced.bank.bank.system.model.Address;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
 
 }
